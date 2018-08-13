e7bd4f648f27aafc978d875b368c735065f39718
Nicolas Dasriaux <nicolas.dasriaux.carbonit@axa-groupsolutions.com>
2018-08-13 19:26:25 +0200

    Draft ADT in second presentation

--- practical-immutability-2.md
+++ practical-immutability-2.md
@@ -2,24 +2,30 @@ autoscale: true
 footer: Practical Immutability
 slidenumbers: true
 
+#  More :wink: Practical
+# [fit] **Immutability**
+## [fit] in Java with _Immutables_ and _Vavr_
+
+---
+
 # Immutability of Variables
 
 * Mutability of **variables** `!=` Mutability of **objects**
-* Immutability of **variables** (local variable, parameter)
-  * Cannot change the variable
-  * `final` vs. ~~`final`~~
 * Immutability of **objects**
   * Cannot mutate the object or collection
   * As seen so far
+* Immutability of **variables** (local variable, parameter)
+  * Cannot change the value (or reference) contained in the variable
+  * `final` vs. ~~`final`~~
 
 ---
 
 # Mutability Combinations
 
-|                          | Immutable Object                     | Mutable Object                                     |
-|--------------------------|--------------------------------------|----------------------------------------------------|
-| **`final` Variable**     | :smile:                              | :fearful:<br/>Mitigate using mutable class locally |
-| **non-`final` Variable** | :neutral_face:<br/>Local effect only | :imp:                                              |
+|                          | Immutable Object             | Mutable Object                                          |
+|--------------------------|------------------------------|---------------------------------------------------------|
+| **`final` Variable**     | :smile:                      | :neutral_face: if stricly local<br/>:fearful: otherwise |
+| **non-`final` Variable** | :neutral_face: stricly local | :imp:                                                   |
 
 ---
 
@@ -89,3 +95,54 @@ switch (color) {
 ---
 
 # [fit] What?
+
+---
+
+# Algebraic Data Type
+
+* Discriminated Union
+* `enum` on steroids
+* ADT for friends
+
+---
+
+# `Direction` enumeration
+
+```java
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+```
+
+---
+
+# Action ADT
+
+```java
+interface Action {
+    @Value.Immutable(singleton = true)
+    class Sleep extends Action {}
+    
+    @Value.Immutable
+    class Walk extends Action {
+        public abstract Direction direction();
+    }
+    
+    @Value.Immutable
+    class Jump extends Action {
+        public abstract Position position();
+    }
+}
+```
+
+ ---
+ 
+# `instanceof` is not as evil as you think
+
+````java
+````
+
+---
--- src/test/java/practicalimmutability/kata/robot/CityMapTest.java
+++ src/test/java/practicalimmutability/kata/robot/CityMapTest.java
@@ -2,42 +2,54 @@ package practicalimmutability.kata.robot;
 
 import io.vavr.collection.List;
 import org.junit.jupiter.api.Test;
-import practicalimmutability.kata.robot.Tile.Booth;
-import practicalimmutability.kata.robot.Tile.Empty;
-import practicalimmutability.kata.robot.Tile.Obstacle;
-import practicalimmutability.kata.robot.Tile.Start;
+import practicalimmutability.kata.robot.Tile.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CityMapTest {
-    private static final CityMap cityMap = CityMap.fromLines(
-            "##########",
-            "#@      B#",
-            "#   XX T #",
-            "#  BN   X#",
-            "# T   I $#",
-            "##########"
+
+    private final CityMap cityMap = CityMap.fromLines(
+            // @formatter:off
+           //0123456789
+            "##########", // 0
+            "#@       #", // 1
+            "# B I T  #", // 2
+            "#        #", // 3
+            "#  N E   #", // 4
+            "#   X    #", // 5
+            "#  W S   #", // 6
+            "#        #", // 7
+            "#   T   $#", // 8
+            "##########"  // 9
+            // @formatter:on
     );
 
     @Test
     void rows() {
-        final CityMap cityMap = CityMap.fromLines(
-                "####",
-                "#@ #",
-                "# $#",
-                "####"
-        );
-
-        final Obstacle O = Obstacle.of();
-        final Start S = Start.of();
-        final Booth B = Booth.of();
-        final Empty e = Empty.of();
+        final Empty __ = Empty.of();
+        final Start A_ = Start.of(); // The Alpha
+        final Booth Z_ = Booth.of(); // The Omega
+        final Obstacle X_ = Obstacle.of();
+        final BreakableObstacle x_ = BreakableObstacle.of();
+        final DirectionModifier N_ = DirectionModifier.of(Direction.North);
+        final DirectionModifier S_ = DirectionModifier.of(Direction.South);
+        final DirectionModifier W_ = DirectionModifier.of(Direction.West);
+        final DirectionModifier E_ = DirectionModifier.of(Direction.East);
+        final CircuitInverter I_ = CircuitInverter.of();
+        final Beer B_ = Beer.of();
+        final Teleporter T_ = Teleporter.of();
 
         final List<List<Tile>> expectedRows = List.of(
-                List.of(O, O, O, O),
-                List.of(O, S, e, O),
-                List.of(O, e, B, O),
-                List.of(O, O, O, O)
+                List.of(X_, X_, X_, X_, X_, X_, X_, X_, X_, X_),
+                List.of(X_, A_, __, __, __, __, __, __, __, X_),
+                List.of(X_, __, B_, __, I_, __, T_, __, __, X_),
+                List.of(X_, __, __, __, __, __, __, __, __, X_),
+                List.of(X_, __, __, N_, __, E_, __, __, __, X_),
+                List.of(X_, __, __, __, x_, __, __, __, __, X_),
+                List.of(X_, __, __, W_, __, S_, __, __, __, X_),
+                List.of(X_, __, __, __, __, __, __, __, __, X_),
+                List.of(X_, __, __, __, T_, __, __, __, Z_, X_),
+                List.of(X_, X_, X_, X_, X_, X_, X_, X_, X_, X_)
         );
 
         assertThat(cityMap.rows()).isEqualTo(expectedRows);
@@ -50,18 +62,18 @@ class CityMapTest {
 
     @Test
     void teleporters() {
-        assertThat(cityMap.teleporters()).containsExactlyInAnyOrder(Position.of(2, 4), Position.of(7, 2));
+        assertThat(cityMap.teleporters()).containsExactlyInAnyOrder(Position.of(6, 2), Position.of(4, 8));
     }
 
     @Test
     void tile() {
-        assertThat(cityMap.tile(Position.of(1, 1))).isEqualTo(Start.of());
+        assertThat(cityMap.tile(Position.of(8, 8))).isEqualTo(Booth.of());
     }
 
     @Test
     void teleporterOutPosition() {
-        assertThat(cityMap.teleporterOutPosition(Position.of(2, 4))).isEqualTo(Position.of(7, 2));
-        assertThat(cityMap.teleporterOutPosition(Position.of(7, 2))).isEqualTo(Position.of(2, 4));
+        assertThat(cityMap.teleporterOutPosition(Position.of(6, 2))).isEqualTo(Position.of(4, 8));
+        assertThat(cityMap.teleporterOutPosition(Position.of(4, 8))).isEqualTo(Position.of(6, 2));
     }
 
     @Test
@@ -69,12 +81,16 @@ class CityMapTest {
         final CityMap expectedCityMap = CityMap.fromLines(
                 "##########",
                 "#@       #",
-                "#   XX T #",
-                "#  BN   X#",
-                "# T   I $#",
+                "# B I T  #",
+                "#        #",
+                "#  N E   #",
+                "#        #",
+                "#  W S   #",
+                "#        #",
+                "#   T   $#",
                 "##########"
         );
 
-        assertThat(cityMap.breakObstacle(Position.of(8, 1))).isEqualTo(expectedCityMap);
+        assertThat(cityMap.breakObstacle(Position.of(4, 5))).isEqualTo(expectedCityMap);
     }
 }
